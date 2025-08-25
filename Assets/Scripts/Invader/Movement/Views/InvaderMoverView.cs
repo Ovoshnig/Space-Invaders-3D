875352fad95177399183dad888166f6b5966c9d9
Ovoshnig <boost45644@gmail.com>
2025-08-25 13:48:29 +0700

    Make an animation of the invaders movement - switching mesh

--- Assets/Scripts/Invader/Movement/Views/InvaderMoverView.cs
+++ Assets/Scripts/Invader/Movement/Views/InvaderMoverView.cs
@@ -3,14 +3,37 @@ using UnityEngine;
 
 public class InvaderMoverView : MonoBehaviour
 {
+    [SerializeField] private Mesh[] _meshes;
+
     private readonly Subject<Unit> _destroyed = new();
 
+    private MeshFilter _meshFilter;
+    private int _meshIndex = 0;
+
     public Observable<Unit> Destroyed => _destroyed;
+
+    private MeshFilter MeshFilter
+    {
+        get
+        {
+            if (_meshFilter == null)
+                _meshFilter = GetComponent<MeshFilter>();
+
+            return _meshFilter;
+        }
+    }
 
     private void OnDestroy() => _destroyed.OnNext(Unit.Default);
 
     public void Move(Vector3 movement)
     {
         transform.Translate(movement);
+
+        _meshIndex++;
+
+        if (_meshIndex >= _meshes.Length)
+            _meshIndex = 0;
+
+        MeshFilter.mesh = _meshes[_meshIndex];
     }
 }
