be581428995ca6235a3928f5fd5dda9247afda82
Ovoshnig <boost45644@gmail.com>
2025-09-06 19:30:40 +0700

    Use a lazy initialized dictionary to map view types to properties in InvaderEntityView

--- Assets/Scripts/Invader/Entity/Views/InvaderEntityView.cs
+++ Assets/Scripts/Invader/Entity/Views/InvaderEntityView.cs
@@ -1,28 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InvaderEntityView : MonoBehaviour
 {
+    private Dictionary<Type, MonoBehaviour> _viewsMap = null;
+
     [field: SerializeField] public InvaderMoverView InvaderMoverView { get; private set; }
     [field: SerializeField] public InvaderShooterView InvaderShooterView { get; private set; }
     [field: SerializeField] public TriggerColliderView InvaderColliderView { get; private set; }
     [field: SerializeField] public InvaderDestroyerView InvaderDestroyerView { get; private set; }
     [field: SerializeField] public InvaderPointsView InvaderPointsView { get; private set; }
 
+    private Dictionary<Type, MonoBehaviour> ViewsMap
+    {
+        get
+        {
+            _viewsMap ??= new Dictionary<Type, MonoBehaviour>
+            {
+                { typeof(InvaderMoverView), InvaderMoverView },
+                { typeof(InvaderShooterView), InvaderShooterView },
+                { typeof(TriggerColliderView), InvaderColliderView },
+                { typeof(InvaderDestroyerView), InvaderDestroyerView },
+                { typeof(InvaderPointsView), InvaderPointsView },
+            };
+
+            return _viewsMap;
+        }
+    }
+
     public T Get<T>() where T : MonoBehaviour
     {
-        Type type = typeof(T);
+        if (ViewsMap.TryGetValue(typeof(T), out MonoBehaviour view))
+            return view as T;
 
-        if (type == typeof(InvaderMoverView))
-            return InvaderMoverView as T;
-        if (type == typeof(InvaderShooterView))
-            return InvaderShooterView as T;
-        if (type == typeof(TriggerColliderView))
-            return InvaderColliderView as T;
-        if (type == typeof(InvaderDestroyerView))
-            return InvaderDestroyerView as T;
-        if (type == typeof(InvaderPointsView))
-            return InvaderPointsView as T;
+        Debug.LogWarning($"View of type {typeof(T)} not found.");
         return null;
     }
 }
